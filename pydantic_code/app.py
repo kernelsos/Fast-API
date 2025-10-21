36a4ce89be67942943c9b979132b8beb9f2299b6
Gagan Sharma <gagansharmadev@gmail.com>
2025-10-21 20:08:28 +0530

    List, Dictionary and Optional keyword

--- pydantic_code/app.py
+++ pydantic_code/app.py
@@ -1,16 +1,24 @@
 from pydantic import BaseModel
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
 
     name : str
     age : int
+    weight: float
+    married: Optional[bool] = False
+    allergies: Optional[List[str]] = None
+    contact_details: Dict[str,str]
 
 def insert_patient(patient: Patient):
     print(patient.name)
     print(patient.age)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
     print("Data insertion successful")
 
-patient_info = {"name": "Gagan", "age": "30"} # Instantiate the pydantic object 
+patient_info = {"name": "Gagan", "age": "30", "weight":168 ,"married": True, "contact_details":{"phno":"703791xxx"}} # Instantiate the pydantic object 
 
 patient1 = Patient(**patient_info) # ** unpacking the dictionary
 
