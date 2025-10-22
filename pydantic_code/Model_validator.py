05b076d917f8422fce8b10f76a82f29e3685c0ad
Gagan Sharma <gagansharmadev@gmail.com>
2025-10-22 16:41:12 +0530

    Model Validator

---
+++ pydantic_code/Model_validator.py
@@ -0,0 +1,51 @@
+from pydantic import BaseModel, Field, EmailStr, AnyUrl, model_validator
+from typing import List, Dict, Optional
+
+# EmailStr command is not working , use command "pip install "pydantic[email]"""
+# model_validator is used when the validation depends on more than two fields.
+#Custom data validation and attaching metadata
+
+class Patient(BaseModel):
+    name: str = Field(..., max_length=50)
+    email: EmailStr
+    linkedin_url: AnyUrl
+    age: int = Field(gt=0, lt=100)
+    weight: float = Field(gt=0)
+    married: Optional[bool] = False
+    allergies: Optional[List[str]] = Field(default=None, max_length=5)
+    contact_details: Dict[str, str]
+
+
+    @model_validator(mode='after')
+    def emergency_contact(cls, model):
+        if model.age > 60 and 'emergency' not in model.contact_details:
+            raise ValueError("Patient older than 60 must have an emergency number")
+        return model
+
+
+
+
+
+def insert_patient(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
+    print(patient.email)
+    print(patient.linkedin_url)
+    print("Data insertion successful")
+
+patient_info = {
+    "name": "Gagan",
+    "email": "raju@gmail.com",
+    "linkedin_url": "https://linkedin.com/in/23453",  
+    "age": 65, 
+    "weight": 168,
+    "married": True,
+    "contact_details": {"phno": "703791xxx","emergency":"47464646"}
+}
+
+# Instantiate and insert
+patient1 = Patient(**patient_info)
+insert_patient(patient1)
