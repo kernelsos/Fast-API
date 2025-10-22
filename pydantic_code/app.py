a42d36a1c40e97b37573646275aff397deb990b3
Gagan Sharma <gagansharmadev@gmail.com>
2025-10-22 13:55:19 +0530

    Emai,url and custom field validation

--- pydantic_code/app.py
+++ pydantic_code/app.py
@@ -1,14 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr, AnyUrl
 from typing import List, Dict, Optional
 
-class Patient(BaseModel):
+# EmailStr command is not working , use command "pip install "pydantic[email]"""
 
-    name : str
-    age : int
-    weight: float
+class Patient(BaseModel):
+    name: str = Field(..., max_length=50)
+    email: EmailStr
+    linkedin_url: AnyUrl
+    age: int = Field(gt=0, lt=100)
+    weight: float = Field(gt=0)
     married: Optional[bool] = False
-    allergies: Optional[List[str]] = None
-    contact_details: Dict[str,str]
+    allergies: Optional[List[str]] = Field(default=None, max_length=5)
+    contact_details: Dict[str, str]
 
 def insert_patient(patient: Patient):
     print(patient.name)
@@ -16,11 +19,20 @@ def insert_patient(patient: Patient):
     print(patient.married)
     print(patient.allergies)
     print(patient.contact_details)
+    print(patient.email)
+    print(patient.linkedin_url)
     print("Data insertion successful")
 
-patient_info = {"name": "Gagan", "age": "30", "weight":168 ,"married": True, "contact_details":{"phno":"703791xxx"}} # Instantiate the pydantic object 
+patient_info = {
+    "name": "Gagan",
+    "email": "raju@gmail.com",
+    "linkedin_url": "https://linkedin.com/in/23453",  
+    "age": 30, 
+    "weight": 168,
+    "married": True,
+    "contact_details": {"phno": "703791xxx"}
+}
 
-patient1 = Patient(**patient_info) # ** unpacking the dictionary
-
+# Instantiate and insert
+patient1 = Patient(**patient_info)
 insert_patient(patient1)
-
