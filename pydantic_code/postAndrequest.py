e38a1ab8687c724c0d0b992b55ecd1ed155273af
Gagan Sharma <gagansharmadev@gmail.com>
2025-10-27 23:47:54 +0530

    update and delete patient

--- pydantic_code/postAndrequest.py
+++ pydantic_code/postAndrequest.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Path, HTTPException, Query
 from fastapi.responses import JSONResponse
 import json
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 
 app = FastAPI()
 
@@ -38,20 +38,29 @@ class Patient(BaseModel):
         else:
             return "Obese"
 
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(gt=0, lt=120, default=None)]
+    gender: Annotated[Optional[Literal['male', 'female', 'others']], Field(default=None)]
+    height: Annotated[Optional[float], Field(gt=0, default=None)]
+    weight: Annotated[Optional[float], Field(gt=0, default=None)]
+
+
 
 # ------------------------------
 # Utility functions
 # ------------------------------
 def load_data():
     try:
-        with open("patients.json", "r") as f:
+        with open("patient.json", "r") as f:
             return json.load(f)
     except FileNotFoundError:
         return {}  # return empty dict if file not found
 
 
 def save_data(data):
-    with open("patients.json", "w") as f:
+    with open("patient.json", "w") as f:
         json.dump(data, f, indent=4)
 
 
@@ -124,3 +133,49 @@ def create_patient(patient: Patient):
     save_data(data)  
 
     return JSONResponse(status_code=201, content={"message": "Patient created successfully"})
+
+
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id: str, patient_update: PatientUpdate):
+
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail="Patient ID not found")
+    
+    existing_patient_info = data[patient_id]
+    # To make things easily convert patient_update object into dictionary using model.dump and unset to include only the fields given by the user not all fields
+
+    updated_patient_info = patient_update.model_dump(exclude_unset=True)
+
+    for key, value in updated_patient_info.items():
+        existing_patient_info[key] = value
+
+    # existing_patient_info -> pydantic object -> updated bmi + verdict -> pydantic object -> dict
+    existing_patient_info['id'] = patient_id
+
+    patient_pydantic_obj = Patient(**existing_patient_info) # Running only this command can give error because you cannot make pydantic model directly from the patients.json dictionary because one id is missing.
+
+    #pydantic object -> dict
+    existing_patient_info = patient_pydantic_obj.model_dump(exclude="id")
+
+    # Add this dict to data
+    data[patient_id] = existing_patient_info
+
+    #save Data
+    save_data()
+
+    return JSONResponse(status_code=200, content={'message': "Patient details updated"})
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
+    #Load data
+    data = load_data()
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail="Patient not found")
+    
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={"message":"Patient deleted"})
